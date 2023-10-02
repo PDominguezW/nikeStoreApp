08338134cb2d7f959415f189033a3768df1e6112
Pablo <pablo.dominguez@uc.cl>
2023-10-02 17:46:55 -0300

    Arreglado

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
     // Function to load the JSON data
     const fetchData = async () => {
       try {
-        const response = await fetch('response.json'); // Replace 'data.json' with your JSON file's path
+        const response = await fetch('https://firebasestorage.googleapis.com/v0/b/melollevo-53056.appspot.com/o/descuentosRunning%2Fresponse.json?alt=media&token=710f564f-dc3a-456e-a4a8-3aa91f76242d&_gl=1*1eflfs4*_ga*NjE4MTA3MjY3LjE2ODg3Mjc5ODQ.*_ga_CW55HF8NVT*MTY5NjI3NzY5NS4zNy4xLjE2OTYyNzc4NjMuNTEuMC4w'); 
         const data = await response.json();
         setBestOffers(data.bestOffers); // Set the state variable to the JSON data
         setBrands(data.brands); // Set the state variable to the JSON data
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './', // Set the root directory of your project,
+  build: {
+    outDir: 'dist', // Specify your output directory here
+    // Add any production-specific configurations here
+  },
 })
